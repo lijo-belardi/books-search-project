6f98f793a05565a09d695228d48e4c0bcdf46382
Lijo <lijo.belardi@icloud.com>
2022-05-06 14:48:35 +0200

    Added webpack's config for Fonts and SVGs

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,11 @@ module.exports = {
                 test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
                 type: 'asset/resource',
             },
+            // Fonts and SVGs
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
         ],
     }
 }
