bef597e36cad28da13e093773ec5f9b3511849d7
Lijo <lijo.belardi@icloud.com>
2022-05-06 12:01:34 +0200

    Defined webpack's output.

--- webpack.config.js
+++ webpack.config.js
@@ -4,4 +4,8 @@ module.exports = {
     entry: {
         main: path.resolve(__dirname, './src/index.js')
     },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
 }
