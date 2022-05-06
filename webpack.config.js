353864da09b1acfc693190861679ac2e736d5e7c
Lijo <lijo.belardi@icloud.com>
2022-05-06 12:06:56 +0200

    fixed entry point's path

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
     entry: {
-        main: path.resolve(__dirname, './src/index.js')
+        main: path.resolve(__dirname, './src/js/index.js')
     },
     output: {
         path: path.resolve(__dirname, './dist'),
