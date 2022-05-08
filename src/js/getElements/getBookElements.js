a04cfb47ca2c1c4e3cb235cf75213c1fa1898885
Lijo <lijo.belardi@icloud.com>
2022-05-08 18:00:32 +0200

    Created function for book's elements

---
+++ src/js/getElements/getBookElements.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import "@babel/runtime/regenerator";
+import { async } from "@babel/runtime/regenerator";
+import { get, isArray, result } from "lodash";
+import {log, logErrors} from "../utility/consoleShortcuts";
+
+
+
+export const getBookElements = async (books) => {
+    try {
+        const bookElements = books.map((book) => {
+            let title = get(book, "title", "Title Unknown")
+            let key = get(book, 'key', 'Key Unknown')
+            let authors = get(book, 'authors', 'Authors Unknown')
+            if (Array.isArray(authors)) {
+                authors.length <= 4
+                    ? authors = authors.map((author) => { return author.name; }).join(", ")
+                    : authors = `${authors[0].name}, ${authors[1].name}, 
+                                 ${authors[2].name}, ${authors[3].name} and others`
+            }
+            return { title, key, authors }
+        })
+        //displayBook(bookElements)
+    } catch (error) {
+        log('ERROR: bookElements function')
+        logErrors(error)
+    }
+}
